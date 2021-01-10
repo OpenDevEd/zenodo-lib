92ec3b453496ef15e9cd59458f0e640399280920
Björn Haßler <bjohas+git@gmail.com>
2021-01-10 22:46:38 +0000

    zotzen-workspace (bumping patch)

--- build/functions.js
+++ build/functions.js
@@ -45,14 +45,14 @@ const opn_1 = __importDefault(require("opn"));
 const helper_1 = require("./helper");
 async function apiCall(args, options, fullResponse = false) {
     console.log(`API CALL`);
-    helper_1.mydebug(args, "apiCall-config(1): args=", args);
-    helper_1.mydebug(args, "apiCall-config(2): options=", options);
-    helper_1.mydebug(args, "apiCall-config(3): fullResponse=", fullResponse);
+    helper_1.mydebug(args, "zenodo-lib/apiCall-config(1): args=", args);
+    helper_1.mydebug(args, "zenodo-lib/apiCall-config(2): options=", options);
+    helper_1.mydebug(args, "zenodo-lib/apiCall-config(3): fullResponse=", fullResponse);
     try {
         const resData = await axios_1.default(options).then(res => {
-            console.log("axios->then");
+            console.log("zenodo-lib/axios->then");
             if ("verbose" in args && args.verbose) {
-                console.log(`response status code: ${res.status}`);
+                console.log(`zenodo-lib/response status code: ${res.status}`);
                 zenodoMessage(res.status);
             }
             if (fullResponse) {
@@ -62,19 +62,19 @@ async function apiCall(args, options, fullResponse = false) {
                 return res.data;
             }
         }).catch(function (err) {
-            console.log("axios->error");
+            console.log("zenodo-lib/axios->error");
             if ("verbose" in args && args.verbose) {
                 console.log(err);
             }
             axiosError(err);
             return null;
         });
-        helper_1.mydebug(args, "apiCall-result: data=", resData);
+        helper_1.mydebug(args, "zenodo-lib/apiCall-result: data=", resData);
         return resData;
     }
     catch (e) {
-        console.log("apiCall-ERROR");
-        helper_1.mydebug(args, "Error in calling axios", e);
+        console.log("zenodo-lib/apiCall-ERROR");
+        helper_1.mydebug(args, "zenodo-lib/Error in calling axios", e);
         return null;
     }
 }
