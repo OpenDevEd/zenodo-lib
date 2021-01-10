5ee36815f1ebb3b492b38b228b61b8b274f0a1a0
Björn Haßler <bjohas+git@gmail.com>
2021-01-10 23:37:02 +0000

    zotzen-workspace (tidy up)

--- src/functions.ts
+++ src/functions.ts
@@ -680,7 +680,7 @@ export async function create(args) {
   const metadata = updateMetadata(args, JSON.parse(blankJson));
   let response_data;
   response_data = await createRecord(args, metadata);
-  console.log("RESP: " + response_data)
+  console.log("RESP: " + JSON.stringify(response_data))
   let response_data_2 = null
   if (response_data) {
     response_data_2 = await finalActions2(args, response_data);
