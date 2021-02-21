dcdf106dc4c3c5c2af5a33d8b465f27961fba3d5
Björn Haßler <bjohas+git@gmail.com>
2021-02-21 16:02:24 +0000

    zotzen-workspace (tidy up)

--- tests/start_here.js
+++ tests/start_here.js
@@ -1,7 +1,12 @@
 const { mainModule } = require("process")
-const zenodolib = require("../build/zenodo-lib.js")
+const zenodo = require("../build/zenodo-lib.js")
 
 async function main() {
+    const about = await zenodo.about({})
+    if (about.env != "sandbox") {
+        console.log("Tests should be run in the sandbox.")
+        process.exit(1)
+    }
     /* {
         console.log("Implicit config.")
         const list = await zenodolib.list({ page: 1, size: 1 })
@@ -16,22 +21,22 @@ async function main() {
     const access_token = "your api key / access token here"
     access_token_v1: {
         console.log("access_token")
-        const list = await zenodolib.list({ access_token: access_token })
+        const list = await zenodo.list({ access_token: access_token })
         console.log(JSON.stringify(list[0], null, 2))
     }
     access_token_v3: {
         console.log("zenodo_access_token")
-        const list = await zenodolib.list({ zenodo_access_token: access_token })
+        const list = await zenodo.list({ zenodo_access_token: access_token })
         console.log(JSON.stringify(list[0], null, 2))
     }
     access_token_v2a: {
         console.log("config_json: { access_token }")
-        const list = await zenodolib.list({ config_json: { access_token: access_token } })
+        const list = await zenodo.list({ config_json: { access_token: access_token } })
         console.log(JSON.stringify(list[0], null, 2))
     }
     access_token_v2b: {
         console.log("config_json: '{ access_token }'")
-        const list = await zenodolib.list({ config_json: `{ "access_token": "${access_token}" }` })
+        const list = await zenodo.list({ config_json: `{ "access_token": "${access_token}" }` })
         console.log(JSON.stringify(list[0], null, 2))
     }
 }
